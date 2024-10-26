4e6fe0b0531618a5e4340873a4be622a6e3826fb
ilyax <157904385+ilyaxuwu@users.noreply.github.com>
2024-10-26 17:26:14 +0300

    Update loadstring.js

--- loadstring.js
+++ loadstring.js
@@ -1,5 +1,5 @@
 function loadstring(url) {
   let script = document.createElement("script"); 
-  script.src = "https://grape-tree-guitar.glitch.me/loveymod0.3.js"; 
+  script.src = url; 
   document.body.appendChild(script);
 }
