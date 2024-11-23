68a4fee253407f59b7549ca51711b44e8070383d
ilyax <157904385+ilyaxuwu@users.noreply.github.com>
2024-11-23 23:50:01 +0300

    Update loadstring.js

--- loadstring.js
+++ loadstring.js
@@ -1,5 +1,10 @@
 function loadstring(url) {
-  let script = document.createElement("script"); 
-  script.src = url; 
-  document.body.appendChild(script);
+    fetch(url)
+        .then(test => test.text())
+        .then(receive => {
+            let script = document.createElement("script");
+            script.innerHTML = receive;
+            document.body.appendChild(script);
+        })
+        .catch(error => console.error('Error: This script is failed to load. The error code is:', error));
 }
